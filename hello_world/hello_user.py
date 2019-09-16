c03d792b8f50eb0ebccc1eaefcaa159ab75bcf6f
Mojola Balogun <meb19@rice.edu>
2019-09-16 16:50:32 -0500

    add base url for hello user

--- hello_world/hello_user.py
+++ hello_world/hello_user.py
@@ -2,8 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/<user>")
+@app.route("/")
 def hello(user):
+    return "Hello, World!"
+
+@app.route("/<user>")
+def hello_user(user):
     return "Hello, %s!" % user
 
 if __name__ == "__main__":
